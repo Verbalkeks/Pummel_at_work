5c66a6066085d4f755a6243d7bfc17b70648b909
verbalkeks <b.sauskojus@freenet.de>
2025-10-28 16:22:30 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/pummel_the_fish/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pummel_the_fish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/data/models/pet.dart
@@ -0,0 +1,21 @@
+enum Species { dog, cat, bird, fish }
+
+class Pet {
+  final String id;
+  final String name;
+  final Species species;
+  final int age;
+  final double weight;
+  final double height;
+  final bool isFemale;
+
+  const Pet({
+    required this.id,
+    required this.name,
+    required this.species,
+    required this.age,
+    required this.weight,
+    required this.height,
+    this.isFemale = true,
+  });
+}
